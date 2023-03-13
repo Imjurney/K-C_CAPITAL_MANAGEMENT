fc664b247f0adac40a267c5bd6e3e39bcca40aa8
Ji-yeon CHOI <jurneyx2@gmail.com>
2023-03-13 13:12:32 +0900

    [feat] TextInput, EmailInput 기능 완성

--- src/components/EmailInput/EmailInput.tsx
+++ src/components/EmailInput/EmailInput.tsx
@@ -1,7 +1,8 @@
 import { TUseForm } from '@formspree/react';
-import { useEffect, useRef, useState } from 'react';
+import { ChangeEvent, useEffect, useRef, useState } from 'react';
 import EamilInputStyle from '@/components/EmailInput/EmailInput.module.css';
 type TodoPreview = Pick<TUseForm[0], 'errors'>;
+
 export function EmailInput() {
   const test = useRef<HTMLInputElement>(null);
   const test2 = useRef<HTMLInputElement>(null);
@@ -10,41 +11,44 @@ export function EmailInput() {
     mail: '',
   });
 
+  const changeMailHandler = (e: ChangeEvent<HTMLInputElement>) => {
+    const { value } = e.target;
+    setEmail({
+      ...email,
+      ['mail']: value,
+    });
+  };
+
+  const changeIdHandler = (e: ChangeEvent<HTMLInputElement>) => {
+    const { value } = e.target;
+    setEmail({
+      ...email,
+      ['id']: value,
+    });
+  };
+
   // eslint-disable-next-line @typescript-eslint/no-empty-function
   useEffect(() => {}, [email]);
 
   return (
     <>
-      <div className={EamilInputStyle.wrapper}>
+      <div className={EamilInputStyle.wrapper} tabIndex={0}>
         <label className={EamilInputStyle.outline_vertical} htmlFor="email">
           Email
         </label>
-        <div>
+        <div className={EamilInputStyle.mail_wrapper}>
           <input
-            className="text-xs indent-3 w-[8rem] h-[2.0625rem]"
-            onChange={(e) => {
-              const { value } = e.target;
-              setEmail({
-                ...email,
-                ['id']: value,
-              });
-            }}
+            className={EamilInputStyle.input}
+            onChange={changeIdHandler}
             ref={test}
             required
             placeholder="Your Email address"
             type="text"
           />
-          <span className="text-base text-kc-contact_gray">&nbsp;@&nbsp;</span>
+          <span className={EamilInputStyle.at_sign}>&nbsp;@&nbsp;</span>
           <input
-            className="text-xs indent-2 w-[8rem] h-[2.0625rem]"
-            onChange={(e) => {
-              const { value } = e.target;
-
-              setEmail({
-                ...email,
-                ['mail']: value,
-              });
-            }}
+            className={EamilInputStyle.input}
+            onChange={changeMailHandler}
             ref={test2}
             required
             placeholder="google.com"
