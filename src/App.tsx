99ff765ba6bc625c1322dd60331f853315df9835
Ji-yeon CHOI <jurneyx2@gmail.com>
2023-03-14 23:13:34 +0900

    [add] reactrouter 연결 완료

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,11 @@ import ContactPage from './pages/Contact/ContactPage';
 function App() {
   return (
     <Routes>
+      <Route path="/" element={<ContactPage />}></Route>
+      <Route path="/about_us" element={<ContactPage />}></Route>
+      <Route path="/investments" element={<ContactPage />}></Route>
+      <Route path="/testimonials" element={<ContactPage />}></Route>
+      <Route path="/other_services" element={<ContactPage />}></Route>
       <Route path="/contact" element={<ContactPage />}></Route>
     </Routes>
   );
